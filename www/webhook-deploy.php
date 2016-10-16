6c4b1a1d33ef7f4919a2a603722dc93775a7df67
Florian Neveu <hello@fnev.eu>
2016-10-16 22:34:13 +0200

    Fix deploy script

--- www/webhook-deploy.php
+++ www/webhook-deploy.php
@@ -5,6 +5,6 @@ const LOCAL_REPO_NAME    = "public_html";
 const REMOTE_REPO        = "git@github.com:fnev-eu/fnev.eu.git";
 const DESIRED_BRANCH     = "master";
 
-echo shell_exec("cd " . LOCAL_ROOT . "/" . LOCAL_REPO_NAME . " && git push origin " . DESIRED_BRANCH);
+echo shell_exec("cd " . LOCAL_ROOT . "/" . LOCAL_REPO_NAME . " && git pull origin " . DESIRED_BRANCH);
 
 die("Deploy done " . mktime());
