ee5000d67533faa1b8c532137d9784eaeffafdec
Florian Neveu <hello@fnev.eu>
2016-10-16 22:09:25 +0200

    test deploy

--- webhook-deploy.php
+++ webhook-deploy.php
@@ -1,15 +1,11 @@
 <?php
 
-$LOCAL_ROOT         = "/var/www";
-$LOCAL_REPO_NAME    = "public_html";
-$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:fnev-eu/fnev.eu.git";
-$DESIRED_BRANCH     = "master";
+const LOCAL_ROOT         = "/var/www";
+const LOCAL_REPO_NAME    = "public_html";
+const LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+const REMOTE_REPO        = "git@github.com:fnev-eu/fnev.eu.git";
+const DESIRED_BRANCH     = "master";
 
-if (file_exists($LOCAL_REPO)) {
-    shell_exec("rm -rf {$LOCAL_REPO}");
-}
-
-echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
+echo shell_exec("cd {LOCAL_REPO} && git push origin {DESIRED_BRANCH}");
 
 die("done " . mktime());
