ccca7a994afe621afc6d8fbbf8520445d73969af
Florian Neveu <hello@fnev.eu>
2019-05-08 23:23:34 +0200

    [symfony] Prepare old version

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
