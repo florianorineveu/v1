1c5c362f5da59ff9791a0704bb0a05129a3709eb
Florian Neveu <hello@fnev.eu>
2019-10-06 21:57:31 +0200

    [back-office] Authentication and first controller for bo

---
+++ src/Controller/Back/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="io_index")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        return $this->redirectToRoute('home');
+        return $this->render('front/legacy.html.twig', []);
+    }
+}
