65cb8f659c89e553c5bf9a3f8a7cd8b6e8944d19
Florian Neveu <hello@fnev.eu>
2019-10-06 22:06:46 +0200

    [back-office] clean code

--- src/Command/CreateAdminCommand.php
+++ src/Command/CreateAdminCommand.php
@@ -33,9 +33,8 @@ class CreateAdminCommand extends Command
     {
         $this
             ->setDescription('Creates a new admin.')
-            ->setHelp('This command allows you to create an admin or a super admin.')
+            ->setHelp('This command allows you to create an admin.')
             ->addArgument('email', InputArgument::REQUIRED, 'E-mail used to create the user.')
-            ->addOption('super-admin', 's', InputOption::VALUE_NONE, 'Create user as super-admin')
         ;
     }
 
@@ -43,13 +42,8 @@ class CreateAdminCommand extends Command
     {
         $io           = new SymfonyStyle($input, $output);
         $email        = $input->getArgument('email');
-        $isSuperAdmin = $input->getOption('super-admin');
 
         $io->title(sprintf('You are about to create an user for %s.', $email));
-
-        if ($isSuperAdmin) {
-            $io->note('This user will be a super admin.');
-        }
 
         $user           = new User();
         $userRepository = $this->em->getRepository(User::class);
@@ -63,12 +57,7 @@ class CreateAdminCommand extends Command
         }
 
         $user->setEmail($email);
-
-        if ($isSuperAdmin) {
-            $user->setRoles(['ROLE_SUPER_ADMIN']);
-        } else {
-            $user->setRoles(['ROLE_ADMIN']);
-        }
+        $user->setRoles(['ROLE_ADMIN']);
 
         $user->setUsername($io->ask('Please enter an username for the user', $email, function ($value) use ($userRepository) {
             if ($userRepository->findOneBy(['username' => $value])) {
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -67,7 +67,6 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
 
         if (!$user) {
-            // fail authentication with a custom error
             throw new CustomUserMessageAuthenticationException('Email could not be found.');
         }
 
