cbe600f0baa950f0b72844cc5ca1d98f82107c07
Florian Neveu <hello@fnev.eu>
2020-02-01 22:48:58 +0100

    Up for preprod

--- src/Controller/Front/DefaultController.php
+++ src/Controller/Front/DefaultController.php
@@ -2,22 +2,15 @@
 
 namespace App\Controller\Front;
 
-use Github\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="legacy")
-     */
-    public function legacy()
-    {
-        return $this->render('front/legacy.html.twig', []);
-    }
-
-    /**
-     * @Route("/index", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
@@ -25,63 +18,13 @@ class DefaultController extends AbstractController
         $now             = new \DateTime();
         $dateDiff        = date_diff($alnilamBirthday, $now);
 
-        /*$client = new Client();
-        $client->authenticate(
-            getenv('GITHUB_SECRET'),
-            null,
-            Client::AUTH_URL_TOKEN
-        );*/
-
-        //$repositories = $client->api('current_user')->repositories('fnev-eu');
-        //$repositories = $client->currentUser()->repositories();;
-
-        /*$commits = $client->api('repo')->commits()->setPerPage(100)->all('fnev-eu', 'Carre-Rose-Films', array('sha' => 'master'));
-
-
-        if ($commits[99]) {
-            $lastCommit = $commits[99];
-            unset($commits[99]);
-            $commits = array_merge($commits, $client->api('repo')->commits()->setPerPage(100)->all('fnev-eu', 'Carre-Rose-Films', array('sha' => $lastCommit['sha'])));
-        }
-
-        dump($commits);*/
-
-        /*$lastCommit = $client->api('repo')->commits()->show('fnev-eu', 'Carre-Rose-Films', 'a8f2def7a25d30bca4465bad10fbc8ca2e10fc8a');
-
-        dump(
-            (new \DateTime($lastCommit['commit']['committer']['date']))->setTimezone(new \DateTimeZone('Europe/Paris'))
-        );
-
-        dump(new \DateTime());
-
-        die();*/
-
         return $this->render('front/index.html.twig', [
             'birthday_count_days' => $dateDiff->days,
         ]);
     }
 
     /**
-     * @Route("/projets/{slug}", name="project")
-     */
-    public function project($slug)
-    {
-        $availableProjects = [
-            'carre-rose',
-            'le-tag-parfait',
-            'newdeal',
-            'wiztopic',
-        ];
-
-        if (!in_array($slug, $availableProjects)) {
-            throw $this->createNotFoundException();
-        }
-
-        return $this->render('front/projects/' . str_replace('-', '_', $slug) . '.html.twig');
-    }
-
-    /**
-     * @Route("/a-propos", name="about")
+     * @Route("/florian", name="about")
      */
     public function about()
     {
@@ -94,5 +37,54 @@ class DefaultController extends AbstractController
     public function uses()
     {
         return $this->render('front/uses.html.twig');
+    }
+
+    /**
+     * @Route("/random", name="random")
+     */
+    public function random(Request $request, RouterInterface $router)
+    {
+        $refererUrl   = $request->headers->get('referer');
+        $referer      = parse_url($refererUrl, PHP_URL_PATH);
+        $refererRoute = $router->match($referer)['_route'];
+
+        $availableRoutes = [
+            'about',
+            'uses',
+            //'case_study',
+            'project_index',
+            'project',
+            'home',
+        ];
+
+        $destination = $availableRoutes[array_rand($availableRoutes)];
+
+        if (!in_array($destination, ['case_study', 'project', $refererRoute])) {
+            return $this->redirectToRoute($destination);
+        }
+
+        if ('case_study' === $destination) {
+            $availableCaseStudies = [
+                'carre-rose',
+                'wiztopic',
+            ];
+
+            return $this->redirectToRoute($destination, [
+                'slug' => $availableCaseStudies[array_rand($availableCaseStudies)]
+            ]);
+        }
+
+        if ('project' === $destination) {
+            $availableProjects = [
+                'athena',
+                'alnilam',
+            ];
+
+            return $this->redirectToRoute($destination, [
+                'slug' => $availableProjects[array_rand($availableProjects)]
+            ]);
+        }
+
+        return $this->redirectToRoute('random');
     }
 }
