642ae33b916680a8b5c1370cc2250beaf424f95d
Florian "Ori" Neveu <hello@fnev.eu>
2022-04-08 01:24:13 +0200

    Init 2.0

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Page;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/{slug}', name: 'home', requirements: ['slug' => '.+'], defaults: ['slug' => null])]
+    public function page(Page $page)
+    {
+        dd($page);
+    }
+
+    /**
+     * @Route("/aleatoire", name="random")
+     */
+    public function random(
+        ProjectRepository $projectRepository,
+        Request $request,
+        RouterInterface $router
+    ) {
+        $refererUrl   = $request->headers->get('referer');
+        $referer      = parse_url($refererUrl, PHP_URL_PATH);
+        $refererRoute = $router->match($referer)['_route'];
+
+        $availableRoutes = [
+            'about',
+            'uses',
+            //'case_study',
+            'project_index',
+            'project',
+            'home',
+        ];
+
+        $destination = $availableRoutes[array_rand($availableRoutes)];
+
+        if (!in_array($destination, ['case_study', 'project', $refererRoute])) {
+            return $this->redirectToRoute($destination);
+        }
+
+        if ('case_study' === $destination) {
+            $availableCaseStudies = [
+                'carre-rose',
+                'wiztopic',
+            ];
+
+            return $this->redirectToRoute($destination, [
+                'slug' => $availableCaseStudies[array_rand($availableCaseStudies)]
+            ]);
+        }
+
+        if ('project' === $destination) {
+            $availableProjects = $projectRepository->findBy([
+                'enabled' => true,
+            ]);
+
+            return $this->redirectToRoute($destination, [
+                'slug' => $availableProjects[array_rand($availableProjects)]->getSlug(),
+            ]);
+        }
+
+        return $this->redirectToRoute('random');
+    }
+
+    /**
+     * @Route("/sitemap.xml", name="sitemap")
+     */
+    public function sitemap(ProjectRepository $projectRepository)
+    {
+        return new Response(
+            $this->renderView('front/sitemap.xml.twig', [
+                'projects' => $projectRepository->findBy([
+                    'enabled' => true,
+                ])
+            ]),
+            Response::HTTP_OK,
+            ['Content-Type' => 'text/xml']
+        );
+    }
+}
---
+++ src/Repository/PageRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Page;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Page|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Page|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Page[]    findAll()
+ * @method Page[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PageRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Page::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Page $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Page $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    // /**
+    //  * @return Page[] Returns an array of Page objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Page
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
