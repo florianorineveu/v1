9ae78f820c06c0c5c5a85ae077854c0e91a0a8e7
Florian "Ori" Neveu <hello@fnev.eu>
2022-04-25 00:31:44 +0200

    [system] Add system informations and more

--- src/Controller/AdminArea/SystemController.php
+++ src/Controller/AdminArea/SystemController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller\AdminArea;
 
+use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 #[Route('/system-audit', name: 'admin_system_audit_')]
 class SystemController extends AbstractController
@@ -18,12 +21,58 @@ class SystemController extends AbstractController
     //'/\[(?P<date>.*)\] (?P<channel>\w+).(?P<level>\w+): (?P<message>.*) (?P<details>[\{(].*?[)\}])?/',
     //'/\[(?P<date>.*)\] (?P<channel>\w+).(?P<level>\w+): (?P<message>.*) (?P<details>(?:[{].*?[}]))/',
 
+    #[Route('/informations', name: 'informations')]
+    public function informations(KernelInterface $kernel)
+    {
+        return $this->render('admin_area/system_audit/informations.html.twig', [
+            'kernel'           => $kernel,
+            'php_version'      => phpversion(),
+            'default_timezone' => date_default_timezone_get(),
+        ]);
+    }
+
+    #[Route('/phpinfo', name: 'phpinfo')]
+    public function phpinfo(RouterInterface $router, Request $request)
+    {
+        $referer = $request->headers->get('referer');
+
+        if ($referer) {
+            $refererPathinfo = Request::create($referer)->getPathInfo();
+            $routeInfos      = $router->match($refererPathinfo);
+            $refererRoute    = $routeInfos['_route'];
+
+            if ('admin_system_audit_informations' === $refererRoute) {
+                ob_start();
+                phpinfo();
+                $phpinfo = ob_get_contents();
+                ob_get_clean();
+
+                return new Response($phpinfo);
+            }
+        }
+
+        throw $this->createAccessDeniedException();
+    }
+
     #[Route('/log', name: 'log', methods: ['GET', 'POST'])]
     public function systemLog(KernelInterface $kernel, Filesystem $filesystem, Request $request): Response {
         $logs       = [];
-        $finder     = new Finder();
-        $file       = $request->request->get('selected_file') ?? $kernel->getEnvironment();
+        $finder     = (new Finder())->in($kernel->getLogDir());
+        $file       = $request->request->get('selected_file');
         $displayAll = $request->request->get('display_all');
+
+        if (!$file) {
+            if ($filesystem->exists($kernel->getLogDir() . '/' . $kernel->getEnvironment() . '.log')) {
+                $file = $kernel->getEnvironment();
+            } else {
+                foreach ($finder as $item) {
+                    if ('log' === $item->getExtension()) {
+                        $file = $item->getFilenameWithoutExtension();
+                        continue;
+                    }
+                }
+            }
+        }
 
         $availableLogFiles = [];
 
@@ -33,10 +82,10 @@ class SystemController extends AbstractController
             }
         }
 
-        if ($filesystem->exists($kernel->getLogDir()) && $filesystem->exists($wantedFile = $kernel->getLogDir() . '/' . $file . '.log')) {
-            $logContent = file_get_contents($wantedFile);
-
-            $explodedLog = explode(PHP_EOL, $logContent);
+        if ($file && $filesystem->exists($wantedFile = $kernel->getLogDir() . '/' . $file . '.log')) {
+            $logContent     = file_get_contents($wantedFile);
+            $explodedLog    = explode(PHP_EOL, $logContent);
+            $logNotReadable = false;
 
             foreach ($explodedLog as $log) {
                 if (!$log) {
@@ -45,7 +94,12 @@ class SystemController extends AbstractController
 
                 preg_match(self::LOG_PATTERN, $log, $data);
 
-                if (!isset($data['date']) || (!$displayAll && in_array($data['level'], ['DEBUG', 'INFO']))) {
+                if (!isset($data['date'])) {
+                    $logNotReadable = true;
+                    continue;
+                }
+
+                if (!$displayAll && in_array($data['level'], ['DEBUG', 'INFO'])) {
                     continue;
                 }
 
@@ -63,6 +117,10 @@ class SystemController extends AbstractController
 
                 $logs[] = $parsedLog;
             }
+
+            if ($logNotReadable) {
+                $this->addFlash('error', 'L\'ensemble des logs n\'a pu être restitué.');
+            }
         } else {
             $this->addFlash('error', 'Une erreur s\'est produite lors de la consultation des logs.');
         }
@@ -72,5 +130,22 @@ class SystemController extends AbstractController
             'available_log_files' => $availableLogFiles,
             'current_file'        => $file,
         ]);
+    }
+
+    #[Route('/delete-log', name: 'delete_log', methods: ['POST'])]
+    public function removeLog(KernelInterface $kernel, Filesystem $filesystem, Request $request): RedirectResponse {
+        if ($file = $request->request->get('selected_file')) {
+            $fileName = $kernel->getLogDir() . '/' . $file . '.log';
+
+            if ($filesystem->exists($fileName)) {
+                $filesystem->remove($fileName);
+
+                $this->addFlash('success', 'Le fichier ' . $file . '.log a bien été effacé.');
+            }
+        } else {
+            $this->addFlash('error', 'Une erreur s\'est produite, veuillez réessayer.');
+        }
+
+        return $this->redirectToRoute('admin_system_audit_log');
     }
 }
